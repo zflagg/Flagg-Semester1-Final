7545f3c231e769228c02e283ac76b866438286b9
Flagg <2006621@student.acalanes.k12.ca.us>
2018-01-10 10:58:36 -0800

    initial commit

---
+++ Computer.java
@@ -0,0 +1,40 @@
+
+public class Computer implements Comparable<Computer>
+{
+    private static int totalComputers;
+    private static int totalModels;
+    
+    public Computer()
+    {
+        
+    }
+    
+    public int getComp() {
+        return totalComputers;
+    }
+    
+    public int getModels() {
+        return totalModels;
+    }
+    
+    public void setComp(int newNumber) {
+        totalComputers = newNumber;
+    }
+    
+    public void setModels(int newModels) {
+        totalModels = newModels;
+    }
+    
+    public void sellComputer() {
+        
+    }
+    
+    public String toString() {
+        
+    }
+    
+    public void compareTo() {
+        
+    }
+        
+}
